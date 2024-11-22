10ba7b91d19cafd6a0aab4f89823a95223208254
Nivedhya S <nivedhyas2006@gmail.com>
2024-11-22 16:40:17 +0530

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char character;
+	printf("enter a character:");
+	scanf("%c",&character);
+	printf("The ASCII value of %c is %d",character,character);
+	return 0;
+}
